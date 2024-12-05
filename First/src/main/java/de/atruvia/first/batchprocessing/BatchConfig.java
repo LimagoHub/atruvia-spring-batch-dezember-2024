c0ff003b3bc000fd77ae686fd3778187ef7d8700
Jo Wagner <jowagner@limago.de>
2024-12-05 11:48:36 +0100

    initial commit

--- First/src/main/java/de/atruvia/first/batchprocessing/BatchConfig.java
+++ First/src/main/java/de/atruvia/first/batchprocessing/BatchConfig.java
@@ -2,6 +2,8 @@ package de.atruvia.first.batchprocessing;
 
 
 import de.atruvia.first.entity.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -9,10 +11,13 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.skip.SkipLimitExceededException;
+import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +26,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Configuration
 public class BatchConfig {
@@ -41,6 +49,7 @@ public class BatchConfig {
         //flatFileItemReader.setLinesToSkip(1);
         return flatFileItemReader;
     }
+
     @Bean
     public PersonItemProcessor processor() {
         return new PersonItemProcessor();
@@ -54,6 +63,7 @@ public class BatchConfig {
                 .dataSource(dataSource)
                 .build();
     }
+
     @Bean
     public Step step1(JobRepository jobRepository,
                       PlatformTransactionManager transactionManager,
@@ -63,12 +73,12 @@ public class BatchConfig {
 
         return new StepBuilder("step1", jobRepository)
 
-                .<Person, Person> chunk(10, transactionManager)
+                .<Person, Person>chunk(10, transactionManager)
                 .reader(reader)
                 .faultTolerant()
-//                .skipLimit(2)
-//                .skip(FlatFileParseException.class)
-//                .noSkip(FileNotFoundException.class)
+                .skipLimit(2)
+                .skip(FlatFileParseException.class)
+                .noSkip(FileNotFoundException.class)
                 //.skipPolicy(createSkipPolicy())
                 .processor(processor)
                 .writer(writer)
@@ -85,4 +95,45 @@ public class BatchConfig {
                 .build();
     }
 
+    @Bean
+    SkipPolicy createSkipPolicy() {
+
+        return new SkipPolicy() {
+
+            private final Logger logger = LoggerFactory.getLogger("badRecordLogger");
+
+            @Override
+            public boolean shouldSkip(final Throwable exception, final long skipCount) throws SkipLimitExceededException {
+
+                if (exception instanceof FileNotFoundException) {
+                    return false;
+                } else if (exception instanceof FlatFileParseException && skipCount <=2) {
+                    FlatFileParseException ffpe = (FlatFileParseException) exception;
+
+                    StringBuilder errorMessage = new StringBuilder();
+                    errorMessage.append("An error occured while processing the ");
+                    errorMessage.append(ffpe.getLineNumber());
+                    errorMessage.append(" line of the file '");
+
+                    Pattern pattern = Pattern.compile(".*(\\W\\w+\\.csv).*");
+                    Matcher matcher = pattern.matcher(ffpe.toString());
+                    if (matcher.matches())
+                        errorMessage.append(matcher.group(1));
+                    else
+                        errorMessage.append("unknown");
+                    errorMessage.append("'. Below was the faulty input.");
+                    errorMessage.append("\n");
+                    errorMessage.append(ffpe.getInput());
+                    errorMessage.append("\n");
+                    logger.error("{}", errorMessage.toString());
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+
+        };
+
+    }
 }
